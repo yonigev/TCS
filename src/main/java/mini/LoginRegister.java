b4887485fd3689efbef53790567ff4ea6e67373c
yonig <yonigev@gmail.com>
2018-06-10 16:17:45 +0300

    Added login\register and Drag&Drop also GUI

--- src/main/java/mini/LoginRegister.java
+++ src/main/java/mini/LoginRegister.java
@@ -59,6 +59,7 @@ public class LoginRegister extends JDialog {
                 onCancel();
             }
         }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+
     }
 
     private void onOK() {
